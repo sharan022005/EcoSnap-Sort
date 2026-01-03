f66960334cf118ba992ac952c84b670abae96739
Sharan Babu <sharan2582005@gmail.com>
2026-01-03 08:50:13 +0000

    im firebase i have opened the analytics but its not showing the details

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
   "projectId": "studio-9287529143-fa9fc",
   "appId": "1:114037958102:web:e34542507cc530f668f077",
+  "storageBucket": "studio-9287529143-fa9fc.appspot.com",
   "apiKey": "AIzaSyBCiBIcTL2D0DJG_qviZF3puiF12hN1ODU",
   "authDomain": "studio-9287529143-fa9fc.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "114037958102",
-  "storageBucket": "studio-9287529143-fa9fc.appspot.com"
+  "measurementId": "G-44521199P2"
 };
